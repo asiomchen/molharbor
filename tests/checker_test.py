6405423f98f412f68d1cbaeffa988cbfa26847f0
Anton Siomchen <41703271+asiomchen@users.noreply.github.com>
2023-12-24 22:02:44 +0100

    Tests modified

--- tests/checker_test.py
+++ tests/checker_test.py
@@ -1,3 +1,4 @@
+from tkinter import N
 import pytest
 from pymolport.checker import Molport, SearchType, UnknownSearchTypeException
 from typing import Iterable
@@ -25,23 +26,30 @@ def test_find_multiple_smiles():
 
     assert isinstance(result, list)
     assert len(result) == len(smiles)
-
-def test_find_invalid_search_type():
+@pytest.mark.parametrize("search_type", [
+    "exact",
+    "similarity",
+    "substructure",
+    "superstructure",
+    10,
+    100,
+])
+def test_find_invalid_search_type(search_type):
     molport = Molport()
     smiles = "C1=CC=CC=C1"
-    search_type = "INVALID"
     max_results = 1000
     similarity = 0.9
 
     with pytest.raises(UnknownSearchTypeException):
         molport.find(smiles, search_type, max_results, similarity)
-
-def test_find_invalid_smiles_type():
+@pytest.mark.parametrize("smiles", [
+    "VCX",
+    "cCcccc123",
+])
+def test_find_invalid_smiles(smiles):
     molport = Molport()
-    smiles = 12345  # Not a string or iterable of strings
     search_type = SearchType.EXACT
     max_results = 1000
     similarity = 0.9
-
-    with pytest.raises(TypeError):
-        molport.find(smiles, search_type, max_results, similarity)
+    result = molport.find(smiles, search_type, max_results, similarity) 
+    assert result == [None]
