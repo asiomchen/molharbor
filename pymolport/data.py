3a8781e42b363074b4e07984c7c191a4488a7d19
Anton Siomchen <41703271+asiomchen@users.noreply.github.com>
2023-12-26 19:31:19 +0100

    Fix formatting issues and update pre-commit hooks

--- pymolport/data.py
+++ pymolport/data.py
@@ -4,103 +4,103 @@ import numpy as np
 
 
 class Molecule(BaseModel):
-    id: int = Field(..., alias='Id')
-    molport_id: str = Field(..., alias='MolPort Id')
-    smiles: str = Field(..., alias='SMILES')
-    canonical_smiles: str = Field(alias='Canonical SMILES')
-    verified_amount: float = Field(alias='Verified Amount')
-    unverified_amount: float = Field(alias='Unverified Amount')
-    similarity_index: float = Field(alias='Similarity Index')
+    id: int = Field(..., alias="Id")
+    molport_id: str = Field(..., alias="MolPort Id")
+    smiles: str = Field(..., alias="SMILES")
+    canonical_smiles: str = Field(alias="Canonical SMILES")
+    verified_amount: float = Field(alias="Verified Amount")
+    unverified_amount: float = Field(alias="Unverified Amount")
+    similarity_index: float = Field(alias="Similarity Index")
 
 
 class Data(BaseModel):
-    molecules: Optional[List[Molecule]] = Field(alias='Molecules')
-    version: str = Field(alias='Version')
+    molecules: Optional[List[Molecule]] = Field(alias="Molecules")
+    version: str = Field(alias="Version")
 
 
 class Result(BaseModel):
-    status: int = Field(alias='Status')
-    message: str = Field(alias='Message')
+    status: int = Field(alias="Status")
+    message: str = Field(alias="Message")
 
 
 class Response(BaseModel):
-    result: Result = Field(alias='Result')
-    data: Data = Field(alias='Data')
+    result: Result = Field(alias="Result")
+    data: Data = Field(alias="Data")
 
 
 class AvailablePacking(BaseModel):
-    Amount: float = Field(np.nan, alias='Amount')
+    Amount: float = Field(np.nan, alias="Amount")
     Measure: str
-    Measure_Id: int = Field(alias='Measure Id')
+    Measure_Id: int = Field(alias="Measure Id")
     Price: float
     Currency: str
-    Currency_Id: int = Field(alias='Currency Id')
-    Delivery_Days: int = Field(alias='Delivery Days')
+    Currency_Id: int = Field(alias="Currency Id")
+    Delivery_Days: int = Field(alias="Delivery Days")
 
 
 class Catalog(BaseModel):
-    Catalog_Id: int = Field(alias='Catalog Id')
-    Catalog_Number: str = Field(alias='Catalog Number')
-    Stock: Optional[float] = Field(None, alias='Stock')
-    Stock_Measure: Optional[str] = Field(None, alias='Stock Measure')
-    Stock_Measure_Id: Optional[int] = Field(None, alias='Stock Measure Id')
-    Purity: str = Field('unknown', alias='Purity')
-    Last_Update_Date: str = Field(alias='Last Update Date')
-    Last_Update_Date_Exact: str = Field(alias='Last Update Date Exact')
-    Available_Packings: List[AvailablePacking] = Field(alias='Available Packings')
+    Catalog_Id: int = Field(alias="Catalog Id")
+    Catalog_Number: str = Field(alias="Catalog Number")
+    Stock: Optional[float] = Field(None, alias="Stock")
+    Stock_Measure: Optional[str] = Field(None, alias="Stock Measure")
+    Stock_Measure_Id: Optional[int] = Field(None, alias="Stock Measure Id")
+    Purity: str = Field("unknown", alias="Purity")
+    Last_Update_Date: str = Field(alias="Last Update Date")
+    Last_Update_Date_Exact: str = Field(alias="Last Update Date Exact")
+    Available_Packings: List[AvailablePacking] = Field(alias="Available Packings")
 
 
 class Supplier(BaseModel):
-    Supplier_Name: str = Field(alias='Supplier Name')
-    Supplier_Id: int = Field(alias='Supplier Id')
-    Minimum_Order: int = Field(alias='Minimum Order')
+    Supplier_Name: str = Field(alias="Supplier Name")
+    Supplier_Id: int = Field(alias="Supplier Id")
+    Minimum_Order: int = Field(alias="Minimum Order")
     Currency: str
-    Currency_Id: int = Field(alias='Currency Id')
+    Currency_Id: int = Field(alias="Currency Id")
     Catalogues: List[Catalog]
 
 
 class BagOfSuppliers(BaseModel):
     Screening_Block_Suppliers: Optional[List[Supplier]] = Field(
-        alias='Screening Block Suppliers'
+        alias="Screening Block Suppliers"
     )
     Building_Block_Suppliers: Optional[List[Supplier]] = Field(
-        alias='Building Block Suppliers'
+        alias="Building Block Suppliers"
     )
-    Virtual_Suppliers: Optional[List[Supplier]] = Field(alias='Virtual Suppliers')
+    Virtual_Suppliers: Optional[List[Supplier]] = Field(alias="Virtual Suppliers")
 
 
 class ShipmentCost(BaseModel):
     Price: float
     Currency: str
-    Currency_Id: int = Field(alias='Currency Id')
-    Location_Type: str = Field(alias='Location Type')
+    Currency_Id: int = Field(alias="Currency Id")
+    Location_Type: str = Field(alias="Location Type")
     Location: str
-    Location_Id: int = Field(alias='Location Id')
-    Shipment_Type: str = Field(alias='Shipment Type')
-    Shipment_Type_Id: int = Field(alias='Shipment Type Id')
+    Location_Id: int = Field(alias="Location Id")
+    Shipment_Type: str = Field(alias="Shipment Type")
+    Shipment_Type_Id: int = Field(alias="Shipment Type Id")
 
 
 class Molecule2(BaseModel):
-    Id: int = Field(..., alias='Id')
-    Molport_Id: str = Field(..., alias='Molport Id')
-    SMILES: str = Field(..., alias='SMILES')
-    Canonical_SMILES: str = Field(..., alias='Canonical SMILES')
+    Id: int = Field(..., alias="Id")
+    Molport_Id: str = Field(..., alias="Molport Id")
+    SMILES: str = Field(..., alias="SMILES")
+    Canonical_SMILES: str = Field(..., alias="Canonical SMILES")
     IUPAC: str
     Formula: str
-    Molecular_Weight: float = Field(..., alias='Molecular Weight')
+    Molecular_Weight: float = Field(..., alias="Molecular Weight")
     Status: str
     Type: str
-    Largest_Stock: str = Field(..., alias='Largest Stock')
-    Largest_Stock_Measure: str = Field(..., alias='Largest Stock Measure')
-    Largest_Stock_Measure_Id: int = Field(..., alias='Largest Stock Measure Id')
-    Catalogues: BagOfSuppliers = Field(..., alias='Catalogues')
+    Largest_Stock: str = Field(..., alias="Largest Stock")
+    Largest_Stock_Measure: str = Field(..., alias="Largest Stock Measure")
+    Largest_Stock_Measure_Id: int = Field(..., alias="Largest Stock Measure Id")
+    Catalogues: BagOfSuppliers = Field(..., alias="Catalogues")
     Synonyms: List[str]
 
 
 class DataSupplier(BaseModel):
-    molecule: Molecule2 = Field(alias='Molecule')
-    version: str = Field(alias='Version')
+    molecule: Molecule2 = Field(alias="Molecule")
+    version: str = Field(alias="Version")
 
 
 class ResponseSupplier(Response):
-    data: DataSupplier = Field(alias='Data')
+    data: DataSupplier = Field(alias="Data")
